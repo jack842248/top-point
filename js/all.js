3579a7310af60d9e2fc4174577f9daf4978ea6ae
jack842248 <72537473+jack842248@users.noreply.github.com>
2020-12-10 16:34:17 +0800

    add page-menu

--- js/all.js
+++ js/all.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    $(".navbar-icon").click(function(){
+        $(".navbar-icon").toggleClass("navbar-icon-click");
+        $(".line1").toggleClass("line1-click");
+        $(".line2").toggleClass("line2-click");
+        $(".line3").toggleClass("line3-click");
+    });
 
 
 
